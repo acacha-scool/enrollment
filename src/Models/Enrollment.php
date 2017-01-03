2062abbcb76aca0dd300de2f943d53ac9a378548
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2017-01-03 18:04:02 +0100

    Create enrollment model

---
+++ src/Models/Enrollment.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Scool\Enrollment\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class Enrollment.
+ *
+ * @package Scool\Enrollment\Models
+ */
+class Enrollment extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+//    protected $fillable = ['name', 'order','study_id'];
+
+}
